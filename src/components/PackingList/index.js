26b1e0d7375b66a7491a00283a4e2b580872dbe3
Casey Hoepner <caseyhoepner@gmail.com>
2018-10-31 23:44:47 -0600

    Write and pass weatherReducer tests

---
+++ src/components/PackingList/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './PackingList.css';
+
+const PackingList = (props) => {
+  return (
+    <div className="pl-container">
+
+    </div>
+  );
+}
+
+export default PackingList;
--- src/reducers/weatherReducer.js
+++ src/reducers/weatherReducer.js
@@ -1,4 +1,4 @@
-export const weatherReducer = (state = {}, action) => {
+export const weatherReducer = (state = [], action) => {
   switch(action.type) {
     case 'SET_WEATHER':
       return {
