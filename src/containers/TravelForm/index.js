8aacc774ad2a952e0634d10fb8d2d1db8b67637d
Casey Hoepner <caseyhoepner@gmail.com>
2018-11-01 20:55:51 -0600

    Style TravelForm

--- src/containers/TravelForm/index.js
+++ src/containers/TravelForm/index.js
@@ -12,7 +12,6 @@ export class TravelForm extends Component {
     super() 
     this.state = {
       city: '',
-      date: ''
     }
   }
 
@@ -37,25 +36,97 @@ export class TravelForm extends Component {
         <Header />
         <form className='tf-form'onSubmit={this.handleSubmit}>
           <h2 className='tf-title'>Where are you going?</h2>
-            <section className='tf-input-components'>
-              <input 
-                className='tf-form-component'
-                onChange={this.handleChange}
-                placeholder='Destination'
-                name='city'
-                value={city}
-              />
-              <input 
-                className='tf-form-component'
-                onChange={this.handleChange}
-                placeholder='Date of Travel'
-                name='date'
-                value={date}
-              />
-              <button className='tf-form-component'>
+            <div className='tf-form-components'>
+              <section className='tf-tomorrow tf-day-input'>
+                <p className='tf-day'>Tomorrow</p>
+                  <input 
+                    className='tf-form-component tf-input-component'
+                    onChange={this.handleChange}
+                    placeholder='Destination'
+                    name='city'
+                    value={city}
+                  />
+              </section>
+              <section 
+                className='tf-day-2 tf-day-input hide'>
+                <p className='tf-day'>Day 2</p>
+                  <input 
+                    className='tf-form-component tf-input-component'
+                    onChange={this.handleChange}
+                    placeholder='Destination'
+                    name='city'
+                    value={city}
+                  />
+              </section>              
+              <section 
+                className='tf-day-3 tf-day-input hide'>
+                <p className='tf-day'>Day 3</p>
+                  <input 
+                    className='tf-form-component tf-input-component'
+                    onChange={this.handleChange}
+                    placeholder='Destination'
+                    name='city'
+                    value={city}
+                  />
+              </section>              
+              <section className='tf-day-4 tf-day-input hide'>
+                <p className='tf-day'>Day 4</p>
+                  <input 
+                    className='tf-form-component tf-input-component'
+                    onChange={this.handleChange}
+                    placeholder='Destination'
+                    name='city'
+                    value={city}
+                  />
+              </section>              
+              <section className='tf-day-5 tf-day-input hide'>
+                <p className='tf-day'>Day 5</p>
+                  <input 
+                    className='tf-form-component tf-input-component'
+                    onChange={this.handleChange}
+                    placeholder='Destination'
+                    name='city'
+                    value={city}
+                  />
+              </section>              
+              <section className='tf-day-6 tf-day-input hide'>
+                <p className='tf-day'>Day 6</p>
+                  <input 
+                    className='tf-form-component tf-input-component'
+                    onChange={this.handleChange}
+                    placeholder='Destination'
+                    name='city'
+                    value={city}
+                  />
+              </section>              
+              <section className='tf-day-7 tf-day-input hide'>
+                <p className='tf-day'>Day 7</p>
+                  <input 
+                    className='tf-form-component tf-input-component'
+                    onChange={this.handleChange}
+                    placeholder='Destination'
+                    name='city'
+                    value={city}
+                  />
+              </section>              
+              <section className='tf-day-8 tf-day-input hide'>
+                <p className='tf-day'>Day 8</p>
+                  <input 
+                    className='tf-form-component tf-input-component'
+                    onChange={this.handleChange}
+                    placeholder='Destination'
+                    name='city'
+                    value={city}
+                  />
+              </section>
+              <button 
+                className='tf-add-btn'>+ Add a Day
+              </button>
+              <button 
+                className='tf-form-component tf-btn'>
                 Get Packing List
               </button>
-            </section>
+            </div>
           </form>
       </div>
     )
