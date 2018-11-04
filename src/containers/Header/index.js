fb2a29810ce53e6176a7911070d5f61b23134bcf
Casey Hoepner <caseyhoepner@gmail.com>
2018-11-04 10:56:16 -0700

    Pass Header tests to 100% coverage

--- src/containers/Header/index.js
+++ src/containers/Header/index.js
@@ -1,19 +1,13 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React from 'react';
 
 import './Header.css'
 
-export class Header extends Component {
-  render() {
-    return (
-      <div className='h-header-container'>
-        <h1 className='h-title'>FannyPack</h1>
-      </div>
-    )
-  }
+const Header = () => {
+  return (
+    <div className='h-header-container'>
+      <h1 className='h-title'>FannyPack</h1>
+    </div>
+  )
 }
 
-const mapDispatchToProps = (dispatch) => ({
-})
-
-export default connect(null, mapDispatchToProps)(Header);
+export default Header;
