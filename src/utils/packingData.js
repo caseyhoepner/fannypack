130f62694b3d94cb3366e49507ce67855788e7b0
Casey Hoepner <caseyhoepner@gmail.com>
2018-11-05 21:13:26 -0700

    Style travel form

--- src/utils/packingData.js
+++ src/utils/packingData.js
@@ -1,16 +1,16 @@
 export const packingData = {
-  'clear-day': ['sunglasses', 'sunscreen', 'hat', 'fanny pack'] ,
-  'clear-night': ['sweater', 'scarf', 'fanny pack'] ,
-  'partly-cloudy-day': ['sunglasses', 'sweater', 'fanny pack'] ,
-  'partly-cloudy-night': ['sweater', 'scarf', 'fanny pack'] ,
-  'cloudy': ['sweater', 'raincoat', 'fanny pack'] ,
-  'rain': ['umbrella', 'raincoat', 'water-resistant shoes', 'fanny pack'] ,
-  'sleet': ['warm coat', 'umbrella', 'fanny pack'] ,
-  'snow': ['warm coat', 'snow boots', 'hat', 'fanny pack'],
-  'wind': ['jacket', 'fanny pack'],
-  'fog': ['jacket', 'scarf', 'fanny pack'],
-  'cold': ['heavy coat', 'close-toed shoes', 'scarf', 'gloves', 'warm hat', 'fanny pack'],
-  'cool': ['jacket', 'warm hat', 'close-toed shoes', 'fanny pack'],
-  'warm': ['short-sleeves', 'open-toed shoes', 'fanny pack'],
-  'hot': ['short-sleeves', 'shorts/skirt', 'open-toed shoes', 'fanny pack']
+  'clear-day': ['sunglasses', 'sunscreen', 'hat', 'jetpack'] ,
+  'clear-night': ['sweater', 'scarf', 'jetpack'] ,
+  'partly-cloudy-day': ['sunglasses', 'sweater', 'jetpack'] ,
+  'partly-cloudy-night': ['sweater', 'scarf', 'jetpack'] ,
+  'cloudy': ['sweater', 'raincoat', 'jetpack'] ,
+  'rain': ['umbrella', 'raincoat', 'water-resistant shoes', 'jetpack'] ,
+  'sleet': ['warm coat', 'umbrella', 'jetpack'] ,
+  'snow': ['warm coat', 'snow boots', 'hat', 'jetpack'],
+  'wind': ['jacket', 'jetpack'],
+  'fog': ['jacket', 'scarf', 'jetpack'],
+  'cold': ['heavy coat', 'close-toed shoes', 'scarf', 'gloves', 'warm hat', 'jetpack'],
+  'cool': ['jacket', 'warm hat', 'close-toed shoes', 'jetpack'],
+  'warm': ['short-sleeves', 'open-toed shoes', 'jetpack'],
+  'hot': ['short-sleeves', 'shorts/skirt', 'open-toed shoes', 'jetpack']
 }
